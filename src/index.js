5cf2de7a52ca1789f1dc802d35f629ba6455bb06
karlakz <yakiyaeva@gmail.com>
2019-10-04 00:00:26 +0600

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+  first_bigint = BigInt(first); // convert first number into BigInt type
+  second_bigint = BigInt(second); // convert second number into BigInt type
+  product = String(first_bigint * second_bigint); // multiply BigInt numbers and convert result into string
+  return product;   
+ }
+ 
